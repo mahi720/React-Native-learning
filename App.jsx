07de0349c5990ca658f7d844001c8bc6dfe06f8b
mahi720 <somammahi@gmail.com>
2025-07-29 18:48:02 +0530

    my first react native code

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 39 }}>App</Text>
+      <Text style={{ fontSize: 30 }}>App21</Text>
+      <Button title="Press Me" />
+    </View>
+  );
+};
+
+export default App;
