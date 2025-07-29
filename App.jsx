79c3c5c93df7c587c7459916750ccc281ddfac4f
mahi720 <somammahi@gmail.com>
2025-07-29 19:13:39 +0530

    leaning about component, create First.jsx component and import to the App.jsx

--- App.jsx
+++ App.jsx
@@ -1,14 +1,28 @@
 import { View, Text, Button } from 'react-native';
 import React from 'react';
+import First from './src/components/First';
 
 const App = () => {
   return (
     <View>
       <Text style={{ fontSize: 39 }}>App</Text>
       <Text style={{ fontSize: 30 }}>App21</Text>
-      <Button title="Press Me" />
+      <Button title="Press Me1" />
+      <First />
+      <First />
+      <First />
+      <First />
     </View>
   );
 };
+
+
+// const Name = () => {
+//   return (
+//     <View>
+//       <Text style={{ fontSize: 28 }}>I'm Mahi</Text>
+//     </View>
+//   )
+// }
 
 export default App;
